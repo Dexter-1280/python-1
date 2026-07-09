4af314f0074ecd90f0cd527f1bc8286fe611c1ea
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-09 20:59:24 +0530

    day 3 of learning pyhton

---
+++ lab0/q4.py
@@ -0,0 +1,5 @@
+"Task 6 of hackerrank"
+#The included code stub will read an integer,n, from STDIN. Without using any string methods, try to print the following
+x1w = int(input("Enter your number:"))
+for i in range(x1w+1):
+    print(i)
