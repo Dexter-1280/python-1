a653d237f2bdad77a91502bdc327e23b6594f7b9
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-16 22:03:37 +0530

    p

--- lab0/q8.py
+++ lab0/q8.py
@@ -9,6 +9,8 @@ if a + b > c and b + c > a and a + c > b:
     print("Area =", area)
 else:
     print("Invalid Triangle")
+ 
+ #2
     n = int(input("Enter n: "))
 
 sum = 0
@@ -17,7 +19,7 @@ for i in range(1, n + 1):
     sum = sum + (2 * i) / i
 
 print("Sum =", sum)
-
+#3
 a = float(input("Enter first parallel side: "))
 b = float(input("Enter second parallel side: "))
 h = float(input("Enter height: "))
@@ -25,3 +27,25 @@ h = float(input("Enter height: "))
 area = ((a + b) * h) / 2
 
 print("Area =", area)
+#4
+pi = 3.14159
+
+r = float(input("Enter radius: "))
+h = float(input("Enter height: "))
+
+volume = pi * r * r * h
+surfaceArea = 2 * pi * r * (r + h)
+
+print("Volume =", volume)
+print("Surface Area =", surfaceArea)
+#5 
+num = float(input("Enter number: "))
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+    #6
+    
