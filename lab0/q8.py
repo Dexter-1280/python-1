60c8594bf37926932e31c9b555d44feccae737d8
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-16 22:05:46 +0530

    python

--- lab0/q8.py
+++ lab0/q8.py
@@ -48,4 +48,48 @@ elif num < 0:
 else:
     print("Zero")
     #6
-    
+    a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if a > b:
+    print("Largest =", a)
+else:
+    print("Largest =", b)
+
+#7
+    num = input("Enter number: ")
+
+if num == num[::-1]:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+#8
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+c = float(input("Enter side c: "))
+
+if a == b == c:
+    print("Equilateral")
+elif a == b or b == c or a == c:
+    print("Isosceles")
+else:
+    print("Scalene")
+#9
+units = int(input("Enter units: "))
+
+if units <= 100:
+    bill = units * 5
+elif units <= 200:
+    bill = 100 * 5 + (units - 100) * 7
+else:
+    bill = 100 * 5 + 100 * 7 + (units - 200) * 10
+
+print("Bill =", bill)
+#10
+age = int(input("Enter age: "))
+test = input("Passed driving test (yes/no): ")
+
+if age >= 18 and test.lower() == "yes":
+    print("Eligible")
+else:
+    print("Not Eligible")
