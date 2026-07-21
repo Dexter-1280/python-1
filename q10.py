fb985e1b53c9791585f430da1b2407bc31492d5c
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-21 23:44:33 +0530

    Software lab

---
+++ q10.py
@@ -0,0 +1,9 @@
+s = "Python Programming"
+
+print(s.lower())
+print(s.title())
+print(s.swapcase())
+print(s.startswith("Python"))
+print(s.endswith("ing"))
+print(s.find("Pro"))
+print(s.count("m"))
