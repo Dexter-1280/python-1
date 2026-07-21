2b0fd6882d6f0607f0d7e9cfd6815f7662e6f3b7
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-21 23:45:02 +0530

    Software lab

--- q10.py
+++ q10.py
@@ -7,3 +7,12 @@ print(s.startswith("Python"))
 print(s.endswith("ing"))
 print(s.find("Pro"))
 print(s.count("m"))
+
+text = "Ramdeobaba University"
+count = 0
+
+for ch in text.lower():
+    if ch in "aeiou":
+        count += 1
+
+print(count)
